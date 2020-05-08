7ef0a0d0e44a2dacb2bb9080e38d70a87f4e7db4
이종섭 <ljs48425707@gmail.com>
2020-05-08 16:22:59 +0900

    2020-05-08 Unity_BlockBlock
    블록 떨어뜨리기

---
+++ BlockBlock/Assets/BlockMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMove : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    bool isMoveRight;
+    GameObject GM;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.gravityScale = 0;
+        isMoveRight = true;
+        GM = GameObject.Find("GameManager");
+    }
+
+    void Update()
+    {
+        if (GM.GetComponent<GameManager>().isTouch == true)
+        {
+            rb2d.gravityScale = 1;
+            GM.GetComponent<GameManager>().isTouch = false;
+            enabled = false;
+        }
+        if (transform.position.x < -3)
+            isMoveRight = true;
+        else if (transform.position.x > 3)
+            isMoveRight = false;
+
+        if (isMoveRight == true)
+            transform.Translate(Vector2.right * 5 * Time.deltaTime);
+        else
+            transform.Translate(Vector2.right * -5 * Time.deltaTime);
+    }
+}
---
+++ BlockBlock/Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isTouch = false;
+    public GameObject blockPrefab;
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            isTouch = true;
+            Instantiate(blockPrefab);
+        }
+    }
+}
